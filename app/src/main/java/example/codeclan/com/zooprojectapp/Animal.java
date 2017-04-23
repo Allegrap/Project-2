ac8e0d2b7aed53b972334d618d9753ca3eadba72
Allegra Perazzo <allegra.perazzo@googlemail.com>
2017-04-23 17:51:21 +0100

    setup of first class Animal

---
+++ app/src/main/java/example/codeclan/com/zooprojectapp/Animal.java
@@ -0,0 +1,24 @@
+package example.codeclan.com.zooprojectapp;
+
+/**
+ * Created by user on 23/04/2017.
+ */
+
+public class Animal {
+
+    private String name;
+    private String type;
+    private char gender;
+    private boolean predator;
+    private String maturity;
+
+    public Animal(String name, String type, char gender, boolean predator, String maturity){
+        this.name = name;
+        this.type = type;
+        this.gender = gender;
+        this.predator = predator;
+        this.maturity = maturity;
+    }
+
+
+}
