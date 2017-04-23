fc31c184b3939a135a447cc0e7d944f4c9a29a76
Allegra Perazzo <allegra.perazzo@googlemail.com>
2017-04-23 18:15:29 +0100

    test file set up

--- app/src/test/java/example/codeclan/com/zooprojectapp/AnimalTest.java
+++ app/src/test/java/example/codeclan/com/zooprojectapp/AnimalTest.java
@@ -1,8 +1,19 @@
 package example.codeclan.com.zooprojectapp;
 
+import org.junit.Before;
+
 /**
  * Created by user on 23/04/2017.
  */
 
 public class AnimalTest {
+
+    Animal animal;
+
+    @Before
+    public void before(){
+        animal = new Animal("Bertie", "rhino", 'm', false, "adult");
+    }
+
+    
 }
