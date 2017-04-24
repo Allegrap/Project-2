9e32ed30ddb915b50480bf1cafeb9c8db0bd7d09
Allegra Perazzo <allegra.perazzo@googlemail.com>
2017-04-24 16:43:27 +0100

    belly added to Animal

--- app/src/main/java/example/codeclan/com/zooprojectapp/Animal.java
+++ app/src/main/java/example/codeclan/com/zooprojectapp/Animal.java
@@ -1,5 +1,7 @@
 package example.codeclan.com.zooprojectapp;
 
+import java.util.ArrayList;
+
 /**
  * Created by user on 23/04/2017.
  */
@@ -10,12 +12,14 @@ public abstract class Animal {
     private String type;
     private char gender;
     private String maturity;
+    private ArrayList<Edible> belly;
 
     public Animal(String name, String type, char gender, String maturity){
         this.name = name;
         this.type = type;
         this.gender = gender;
         this.maturity = maturity;
+        this.belly = new ArrayList<Edible>();
     }
 
 
