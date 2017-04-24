e620396daede3bab51b5c5bf107c7f7433f56980
Allegra Perazzo <allegra.perazzo@googlemail.com>
2017-04-24 16:10:24 +0100

    Carnivore Class added

---
+++ app/src/main/java/example/codeclan/com/zooprojectapp/Carnivore.java
@@ -0,0 +1,12 @@
+package example.codeclan.com.zooprojectapp;
+
+/**
+ * Created by user on 24/04/2017.
+ */
+
+public class Carnivore extends Animal {
+
+    public Carnivore(String name, String type, char gender, String maturity){
+        super(name, type, gender, maturity);
+    }
+}
