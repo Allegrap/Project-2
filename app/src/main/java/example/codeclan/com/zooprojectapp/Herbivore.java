3e76341e51f61434836dd62f6b9cca7bf239adad
Allegra Perazzo <allegra.perazzo@googlemail.com>
2017-04-24 16:09:29 +0100

    Herbivore file set up

---
+++ app/src/main/java/example/codeclan/com/zooprojectapp/Herbivore.java
@@ -0,0 +1,12 @@
+package example.codeclan.com.zooprojectapp;
+
+/**
+ * Created by user on 24/04/2017.
+ */
+
+public class Herbivore extends Animal {
+
+    public Herbivore(String name, String type, char gender, String maturity){
+        super(name, type, gender, maturity);
+    }
+}
