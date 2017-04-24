dfa6c83001a589d12f9fe8df59cb6c7943ccae83
Allegra Perazzo <allegra.perazzo@googlemail.com>
2017-04-24 15:44:18 +0100

    food class made abstract and implements Edible

--- app/src/main/java/example/codeclan/com/zooprojectapp/Food.java
+++ app/src/main/java/example/codeclan/com/zooprojectapp/Food.java
@@ -4,5 +4,16 @@ package example.codeclan.com.zooprojectapp;
  * Created by user on 24/04/2017.
  */
 
-public class Food implements Edible{
+public abstract class Food implements Edible{
+
+    int nutritionValue;
+
+    public Food(int nutritionValue){
+        this.nutritionValue = nutritionValue;
+    }
+
+    public int getNutritionValue(){
+        return this.nutritionValue;
+    }
+
 }
