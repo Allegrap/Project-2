c25af9db6bd6f8956eef5e3e6dfb561ecf694b90
Allegra Perazzo <allegra.perazzo@googlemail.com>
2017-04-24 16:33:05 +0100

    FoodType enum added

---
+++ app/src/main/java/example/codeclan/com/zooprojectapp/FoodType.java
@@ -0,0 +1,10 @@
+package example.codeclan.com.zooprojectapp;
+
+/**
+ * Created by user on 24/04/2017.
+ */
+
+public enum FoodType{
+    MEAT,
+    GRASS
+}
---
+++ app/src/test/java/example/codeclan/com/zooprojectapp/FoodTest.java
@@ -0,0 +1,14 @@
+package example.codeclan.com.zooprojectapp;
+
+import org.junit.Before;
+
+/**
+ * Created by user on 24/04/2017.
+ */
+
+public class FoodTest {
+
+
+    }
+
+}
