059eb3b3db690b3f373ce5c29f12dbe4fd0055d1
Allegra Perazzo <allegra.perazzo@googlemail.com>
2017-04-25 17:01:29 +0100

    donateStray test added to visitor

--- app/src/test/java/example/codeclan/com/zooprojectapp/VisitorTest.java
+++ app/src/test/java/example/codeclan/com/zooprojectapp/VisitorTest.java
@@ -3,6 +3,8 @@ package example.codeclan.com.zooprojectapp;
 import org.junit.Before;
 import org.junit.Test;
 
+import example.codeclan.com.zooprojectapp.food_management.FoodType;
+import example.codeclan.com.zooprojectapp.food_management.Stray;
 import example.codeclan.com.zooprojectapp.zoo_management.Visitor;
 import example.codeclan.com.zooprojectapp.zoo_management.Zoo;
 
@@ -16,11 +18,13 @@ public class VisitorTest {
 
     Visitor visitor;
     Zoo zoo;
+    Stray stray;
 
     @Before
     public void before(){
         visitor = new Visitor("Allegra", 400);
         zoo = new Zoo(20, 5000);
+        stray = new Stray("dog", FoodType.MEAT, 30);
     }
 
     @Test
@@ -37,6 +41,12 @@ public class VisitorTest {
     public void canPayEntryFee(){
         visitor.payEntryFee(zoo.getEntryFee());
         assertEquals(380, visitor.getFunds());
+    }
+
+    @Test
+    public void canDonateStray(){
+        visitor.donateStray(stray);
+        assertEquals(1, zoo.strayCount());
     }
 
 }
