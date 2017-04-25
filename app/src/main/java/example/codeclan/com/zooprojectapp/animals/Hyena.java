54f030d2d3149e20fe26dd95fbb86e7a6460fe58
Allegra Perazzo <allegra.perazzo@googlemail.com>
2017-04-25 17:22:02 +0100

    Scavengeable interface set up and scavenge method added to Hyena

--- app/src/main/java/example/codeclan/com/zooprojectapp/animals/Hyena.java
+++ app/src/main/java/example/codeclan/com/zooprojectapp/animals/Hyena.java
@@ -1,16 +1,18 @@
 package example.codeclan.com.zooprojectapp.animals;
 
+import example.codeclan.com.zooprojectapp.Interfaces.Scavengeable;
+
 /**
  * Created by user on 25/04/2017.
  */
 
-public class Hyena extends Carnivore implements Scavangeable{
+public class Hyena extends Carnivore implements Scavengeable {
 
     public Hyena(String name, char gender, String maturity){
         super(name, gender, maturity);
     }
 
-    public void scavenge(){
+    public String scavenge(){
         return "I am a scavenger";
     }
 
