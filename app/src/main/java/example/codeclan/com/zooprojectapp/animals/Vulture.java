9ab5366ed35593b4e05283677b8601e19590d2c8
Allegra Perazzo <allegra.perazzo@googlemail.com>
2017-04-25 17:17:31 +0100

    Flyable set up and fly method added to Vulture

--- app/src/main/java/example/codeclan/com/zooprojectapp/animals/Vulture.java
+++ app/src/main/java/example/codeclan/com/zooprojectapp/animals/Vulture.java
@@ -1,5 +1,7 @@
 package example.codeclan.com.zooprojectapp.animals;
 
+import example.codeclan.com.zooprojectapp.Interfaces.Flyable;
+
 /**
  * Created by user on 25/04/2017.
  */
@@ -8,6 +10,10 @@ public class Vulture extends Carnivore implements Flyable {
 
     public Vulture(String name, char gender, String maturity){
         super(name, gender, maturity);
+    }
+
+    public String fly(){
+        return "I can fly!";
     }
 
 }
