96d5df7fc1f0d9d6e95a677a6e3910fbe77c6ca2
Allegra Perazzo <allegra.perazzo@googlemail.com>
2017-04-25 14:09:06 +0100

    ZooTest added and set up

---
+++ app/src/test/java/example/codeclan/com/zooprojectapp/ZooTest.java
@@ -0,0 +1,36 @@
+package example.codeclan.com.zooprojectapp;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import example.codeclan.com.zooprojectapp.animals.Carnivore;
+import example.codeclan.com.zooprojectapp.animals.Herbivore;
+import example.codeclan.com.zooprojectapp.zoo_management.Visitor;
+import example.codeclan.com.zooprojectapp.zoo_management.Zoo;
+
+/**
+ * Created by user on 25/04/2017.
+ */
+
+public class ZooTest {
+
+    Zoo zoo;
+    Herbivore herbivore;
+    Carnivore carnivore;
+    Visitor visitor;
+    Visitor visitor2;
+
+    @Before
+    public void before(){
+        zoo = new Zoo(20);
+        herbivore = new Herbivore("Bertie", "rhino", 'm', "adult");
+        carnivore = new Carnivore("Darren", "leopard", 'm', "baby");
+        visitor = new Visitor("Allegra", 400);
+        visitor2 = new Visitor("Chiara", 10);
+    }
+
+    
+
+
+
+}
