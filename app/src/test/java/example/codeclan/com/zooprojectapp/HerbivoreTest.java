44eb5fc8ea465f0994dfcbbc7e7b1c2e5c1b3506
Allegra Perazzo <allegra.perazzo@googlemail.com>
2017-04-25 10:03:51 +0100

    cannotEatMeat and canEatVegetables added to new HerbivoreTest file

--- app/src/test/java/example/codeclan/com/zooprojectapp/HerbivoreTest.java
+++ app/src/test/java/example/codeclan/com/zooprojectapp/HerbivoreTest.java
@@ -9,29 +9,29 @@ import static junit.framework.Assert.assertEquals;
  * Created by user on 25/04/2017.
  */
 
-public class CarnivoreTest {
+public class HerbivoreTest {
 
-    Carnivore carnivore;
+    Herbivore herbivore;
     Food meat;
     Food vegetables;
 
     @Before
     public void before(){
-        carnivore = new Carnivore("Angela", "lion", 'f', "adult");
+        herbivore = new Herbivore("Tony", "turtle", 'm', "adult");
         meat = new Food(FoodType.MEAT, 9);
         vegetables = new Food(FoodType.VEGETABLES, 4);
     }
 
     @Test
-    public void canEatMeat(){
-        carnivore.eat(meat);
-        assertEquals(1, carnivore.getBellySize());
+    public void canEatGrass(){
+        herbivore.eat(vegetables);
+        assertEquals(0, herbivore.getBellySize());
     }
 
     @Test
-    public void cannotEatVegetables(){
-        carnivore.eat(vegetables);
-        assertEquals(0, carnivore.getBellySize());
+    public void cannotEatMeat(){
+        herbivore.eat(meat);
+        assertEquals(1, herbivore.getBellySize());
     }
 
 }
