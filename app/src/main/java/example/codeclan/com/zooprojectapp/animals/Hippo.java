c35c59f58aee82f76173dbcfb4779269d1832e1a
Allegra Perazzo <allegra.perazzo@googlemail.com>
2017-04-25 16:09:50 +0100

    Hippo class added

---
+++ app/src/main/java/example/codeclan/com/zooprojectapp/animals/Hippo.java
@@ -0,0 +1,13 @@
+package example.codeclan.com.zooprojectapp.animals;
+
+/**
+ * Created by user on 25/04/2017.
+ */
+
+public class Hippo extends Herbivore{
+
+    public Hippo(String name, String type, char gender, String maturity){
+        super(name, type, gender, maturity);
+    }
+
+}
