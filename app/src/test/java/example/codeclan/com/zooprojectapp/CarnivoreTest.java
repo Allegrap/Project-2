98dd2979d52be04f2c3cc4df76b9b29850b300bc
Allegra Perazzo <allegra.perazzo@googlemail.com>
2017-04-25 10:09:49 +0100

    grass test added to CarnivoreTest and passes

--- app/src/test/java/example/codeclan/com/zooprojectapp/CarnivoreTest.java
+++ app/src/test/java/example/codeclan/com/zooprojectapp/CarnivoreTest.java
@@ -14,17 +14,25 @@ public class CarnivoreTest {
     Carnivore carnivore;
     Food meat;
     Food vegetables;
+    Food grass;
 
     @Before
     public void before(){
         carnivore = new Carnivore("Angela", "lion", 'f', "adult");
         meat = new Food(FoodType.MEAT, 9);
         vegetables = new Food(FoodType.VEGETABLES, 4);
+        grass = new Food(FoodType.GRASS, 3);
     }
 
     @Test
     public void canEatMeat(){
         carnivore.eat(meat);
+        assertEquals(1, carnivore.getBellySize());
+    }
+
+    @Test
+    public void canEatGrass(){
+        carnivore.eat(grass);
         assertEquals(1, carnivore.getBellySize());
     }
 
