79e1cfe26268c23d73c6b0e3973ec4d2b6ca166e
Allegra Perazzo <allegra.perazzo@googlemail.com>
2017-04-25 16:37:37 +0100

    canGetType test added

--- app/src/test/java/example/codeclan/com/zooprojectapp/StrayTest.java
+++ app/src/test/java/example/codeclan/com/zooprojectapp/StrayTest.java
@@ -1,9 +1,12 @@
 package example.codeclan.com.zooprojectapp;
 
 import org.junit.Before;
+import org.junit.Test;
 
 import example.codeclan.com.zooprojectapp.food_management.FoodType;
 import example.codeclan.com.zooprojectapp.food_management.Stray;
+
+import static junit.framework.Assert.assertEquals;
 
 /**
  * Created by user on 25/04/2017.
@@ -17,6 +20,12 @@ public class StrayTest {
     public void before(){
         stray = new Stray("dog", FoodType.MEAT, 30);
     }
-    
+
+    @Test
+    public void canGetTypeTest(){
+        assertEquals("dog", stray.getType());
+    }
+
+
 
 }
