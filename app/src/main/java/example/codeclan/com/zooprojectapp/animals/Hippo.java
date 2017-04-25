0930a2175b5e1c83cc8127b1b820f6433aef8f48
Allegra Perazzo <allegra.perazzo@googlemail.com>
2017-04-25 16:27:38 +0100

    Swimmable implemented by hippo

--- app/src/main/java/example/codeclan/com/zooprojectapp/animals/Hippo.java
+++ app/src/main/java/example/codeclan/com/zooprojectapp/animals/Hippo.java
@@ -1,13 +1,19 @@
 package example.codeclan.com.zooprojectapp.animals;
 
+import example.codeclan.com.zooprojectapp.Interfaces.Swimmable;
+
 /**
  * Created by user on 25/04/2017.
  */
 
-public class Hippo extends Herbivore{
+public class Hippo extends Herbivore implements Swimmable{
 
     public Hippo(String name, char gender, String maturity){
         super(name, gender, maturity);
+    }
+
+    public String swim(){
+        return "I am swimming!";
     }
 
 }
