1adf84cd3e29bec928f4316c8ba70c441219d283
Allegra Perazzo <allegra.perazzo@googlemail.com>
2017-04-25 16:29:03 +0100

    Leopard implements Climbable

--- app/src/main/java/example/codeclan/com/zooprojectapp/animals/Leopard.java
+++ app/src/main/java/example/codeclan/com/zooprojectapp/animals/Leopard.java
@@ -1,13 +1,19 @@
 package example.codeclan.com.zooprojectapp.animals;
 
+import example.codeclan.com.zooprojectapp.Interfaces.Climbable;
+
 /**
  * Created by user on 25/04/2017.
  */
 
-public class Leopard extends Carnivore {
+public class Leopard extends Carnivore implements Climbable{
 
     public Leopard(String name, char gender, String maturity){
         super(name, gender, maturity);
+    }
+
+    public String climb(){
+        return "I can climb!";
     }
 
 }
