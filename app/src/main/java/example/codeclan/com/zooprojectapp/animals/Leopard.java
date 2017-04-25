2eb68dd8242babe7be82d48a2128d197043f409e
Allegra Perazzo <allegra.perazzo@googlemail.com>
2017-04-25 16:13:19 +0100

    Leopard class added

---
+++ app/src/main/java/example/codeclan/com/zooprojectapp/animals/Leopard.java
@@ -0,0 +1,13 @@
+package example.codeclan.com.zooprojectapp.animals;
+
+/**
+ * Created by user on 25/04/2017.
+ */
+
+public class Leopard extends Carnivore {
+
+    public Leopard(String name, char gender, String maturity){
+        super(name, gender, maturity);
+    }
+    
+}
