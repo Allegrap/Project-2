5f33e4aa316af3f30fffe1a5b753c01070bae7ee
Allegra Perazzo <allegra.perazzo@googlemail.com>
2017-04-25 16:33:41 +0100

    Strays class added which implements Edible

---
+++ app/src/main/java/example/codeclan/com/zooprojectapp/food_management/Strays.java
@@ -0,0 +1,28 @@
+package example.codeclan.com.zooprojectapp.food_management;
+
+import example.codeclan.com.zooprojectapp.Interfaces.Edible;
+
+/**
+ * Created by user on 25/04/2017.
+ */
+
+public class Strays implements Edible {
+
+    private String type;
+    private FoodType foodType;
+    private int nutritionalValue;
+
+    public Strays(String type, FoodType foodType, int nutritionValue){
+        this.type = type;
+        this.foodType = foodType;
+        this.nutritionalValue = nutritionValue;
+    }
+
+    public int getNutritionalValue(){
+            return this.nutritionalValue;
+        }
+
+    public FoodType getFoodType() {
+            return foodType;
+        }
+}
