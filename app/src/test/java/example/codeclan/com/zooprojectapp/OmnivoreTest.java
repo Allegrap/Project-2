e78b481b3d3aa162ce38a544900fd680c6d47a82
Allegra Perazzo <allegra.perazzo@googlemail.com>
2017-04-26 10:56:21 +0100

    Test file updated and canEatVegetables test added

--- app/src/test/java/example/codeclan/com/zooprojectapp/OmnivoreTest.java
+++ app/src/test/java/example/codeclan/com/zooprojectapp/OmnivoreTest.java
@@ -1,8 +1,14 @@
 package example.codeclan.com.zooprojectapp;
 
 import org.junit.Before;
+import org.junit.Test;
 
 import example.codeclan.com.zooprojectapp.animals.BeardedDragon;
+import example.codeclan.com.zooprojectapp.animals.Leopard;
+import example.codeclan.com.zooprojectapp.food_management.Food;
+import example.codeclan.com.zooprojectapp.food_management.FoodType;
+
+import static junit.framework.Assert.assertEquals;
 
 /**
  * Created by user on 26/04/2017.
@@ -11,10 +17,26 @@ import example.codeclan.com.zooprojectapp.animals.BeardedDragon;
 public class OmnivoreTest {
 
     BeardedDragon beardedDragon;
+    Food meat;
+    Food vegetables;
+    Food grass;
+    Food fruit;
+    Food fish;
 
     @Before
     public void before(){
         beardedDragon = new BeardedDragon("Tyrone", 'f', "adult");
+        meat = new Food(FoodType.MEAT, 9);
+        fish = new Food(FoodType.FISH, 8);
+        vegetables = new Food(FoodType.VEGETABLES, 4);
+        grass = new Food(FoodType.GRASS, 3);
+        fruit = new Food(FoodType.FRUIT, 5);
+    }
+
+    @Test
+    public void canEatVegetables(){
+        beardedDragon.eat(vegetables);
+        assertEquals(1, beardedDragon.getBellySize());
     }
 
 }
