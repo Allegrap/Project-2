9c75420dbaf01825fa66f4b9839c71fb886aeb0e
Allegra Perazzo <allegra.perazzo@googlemail.com>
2017-04-26 09:10:07 +0100

    Scavengeable implemented by Vulture

--- app/src/main/java/example/codeclan/com/zooprojectapp/animals/Vulture.java
+++ app/src/main/java/example/codeclan/com/zooprojectapp/animals/Vulture.java
@@ -1,12 +1,13 @@
 package example.codeclan.com.zooprojectapp.animals;
 
 import example.codeclan.com.zooprojectapp.Interfaces.Flyable;
+import example.codeclan.com.zooprojectapp.Interfaces.Scavengeable;
 
 /**
  * Created by user on 25/04/2017.
  */
 
-public class Vulture extends Carnivore implements Flyable {
+public class Vulture extends Carnivore implements Flyable, Scavengeable {
 
     public Vulture(String name, char gender, String maturity){
         super(name, gender, maturity);
@@ -14,6 +15,10 @@ public class Vulture extends Carnivore implements Flyable {
 
     public String fly(){
         return "I can fly!";
+    }
+
+    public String scavenge(){
+        return "I am a scavenger!";
     }
 
 }
