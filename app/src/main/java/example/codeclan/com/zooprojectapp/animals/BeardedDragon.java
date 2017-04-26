26513c23d3b210dee7e6b43bd8f9ae06bf7009e0
Allegra Perazzo <allegra.perazzo@googlemail.com>
2017-04-26 10:50:56 +0100

    BeardedDragon added

---
+++ app/src/main/java/example/codeclan/com/zooprojectapp/animals/BeardedDragon.java
@@ -0,0 +1,35 @@
+package example.codeclan.com.zooprojectapp.animals;
+
+import example.codeclan.com.zooprojectapp.Interfaces.Climbable;
+import example.codeclan.com.zooprojectapp.Interfaces.Edible;
+import example.codeclan.com.zooprojectapp.food_management.FoodType;
+
+/**
+ * Created by user on 26/04/2017.
+ */
+
+public class BeardedDragon extends Omnivore implements Climbable {
+
+    public BeardedDragon(String name, char gender, String maturity){
+        super(name, gender, maturity);
+    }
+
+    public String climb(){
+        return "I can climb!";
+    }
+
+    @Override
+    public void eat(Edible food){
+        if(food.getFoodType() == FoodType.INSECTS){
+            belly.add(food);
+        }
+        else if(food.getFoodType() == FoodType.GRASS){
+            belly.add(food);
+        }
+        else if(food.getFoodType() == FoodType.VEGETABLES){
+            belly.add(food);
+        }
+        return;
+    }
+
+}
