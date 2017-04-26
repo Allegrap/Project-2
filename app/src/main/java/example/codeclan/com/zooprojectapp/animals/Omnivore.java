6dee3634c2513eaf8d5d187be1c2995e226229d2
Allegra Perazzo <allegra.perazzo@googlemail.com>
2017-04-26 10:43:18 +0100

    Omnivore class added

---
+++ app/src/main/java/example/codeclan/com/zooprojectapp/animals/Omnivore.java
@@ -0,0 +1,13 @@
+package example.codeclan.com.zooprojectapp.animals;
+
+/**
+ * Created by user on 26/04/2017.
+ */
+
+public class Omnivore extends Animal{
+
+    public Omnivore(String name, char gender, String maturity){
+        super(name, gender, maturity);
+    }
+
+}
